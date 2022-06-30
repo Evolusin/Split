806011a2604a7fd60962e83f68e62c099267f2ba
evoar <evoarcadias@gmail.com>
2022-06-30 21:17:12 +0200

    New class in users -> PasswordChange

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your models here.
+
+# class PasswordChange(models.Model):
+#     """This middleware checks if the user requires a password change and if he needs then forces him to change it"""
+#     def __init__(self):
+#         self.get_response = get_response
+#
+#     def __call__(self, request):
