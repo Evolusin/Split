2924b09b63b893c42796027e35372105e49b2189
Evolusin <98921577+Evolusin@users.noreply.github.com>
2022-07-11 18:33:24 +0200

    Debug settings to heroku

--- Split/settings.py
+++ Split/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9-m*f4+14)k)xzv&s%gl_v8*5mm10=%!yno&f$b@rr51=-ei8@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
