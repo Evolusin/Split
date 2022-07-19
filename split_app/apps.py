1b621ce346a5c79522d0aeb62972ab59a6a0e56a
evoar <evoarcadias@gmail.com>
2022-07-19 12:11:49 +0200

    Comment for app config

--- split_app/apps.py
+++ split_app/apps.py
@@ -5,5 +5,6 @@ class SplitAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "split_app"
     # config for signals
+
     def ready(self):
         import split_app.signals
